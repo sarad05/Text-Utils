a961fe4b9f930857e2dca867103d8bb5d4b85cda
Sarad <saradrana12@gmail.com>
2021-08-28 17:17:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
-
+//  import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+// import{
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+// }from "react-router-dom";
 function App() {
+  const [mode, setMode] = useState('light');  //whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+  const showAlert=(message,type)=>{
+     setAlert({
+       msg:message,
+       type:type
+     })
+     setTimeout(()=>{
+        setAlert(null);
+     },1500);
+  }
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.background='#042743';
+      showAlert("Dark mode has been enabled","success");
+      // setInterval(()=>{
+      //    document.title=" TextUtils is amazing";
+      // },2000);
+      // setInterval(()=>{
+      //    document.title="Install TextUtils Now";
+      // },1500);
+    }
+    else{
+      setMode('light');
+      document.body.style.background='white';
+      showAlert("Light mode has been enabled","success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+{/* <Navbar title="TextUtils" About="About Utils"/> */}
+{/* <Router> */}
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert}/>
+<div className="container my-3">
+{/* <Switch> */}
+{/* /users------> Component-1 */}
+{/* /users/home-----> Component-2 */}
+          {/* <Route exact path="/about">
+            <About />
+          </Route> */}
+          {/* <Route exact path="/"> */}
+          <TextForm  showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces " mode={mode}/> 
+          {/* </Route> */}
+  {/* </Switch> */}
+  </div>
+  {/* </Router> */}
+
+</>
+  )
+ 
 }
 
 export default App;
